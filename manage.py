f277be93645eb8a1ba3503c818fe55d915363657
Kaustubh Bhargava <78357870+Kaustubh05334@users.noreply.github.com>
2023-11-26 16:32:04 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Out_of_Ghar.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Global_Trotter.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
